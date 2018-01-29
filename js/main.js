2badeb1ba09875768edb943cec3cf387d801f25d
mozukraken <mozukraken@gmail.com>
2018-01-29 23:31:18 +0900

    ボックスを動かした

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(function() {
   $('#container').mousemove(function(e) {
     console.log(e.clientX, e.clientY);
+    var cx = $(this).width() / 2;
+    var cy = $(this).height() / 2;
+    var dx = e.clientX - cx;
+    var dy = e.clientY - cy;
+
+    $('#box1').css('left', cx + dx * 1.1);
+    $('#box2').css('left', cx + dx * 1.3);
+    $('#box3').css('left', cx + dx * 1.7);
+    $('#box1').css('top', cy + dy * 1.0);
+    $('#box2').css('top', cy + dy * 1.2);
+    $('#box3').css('top', cy + dy * 1.5);
   });
 });
